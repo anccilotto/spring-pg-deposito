ef5e500dcc5edda6886b3dfe61d01262e1168108
anccilotto <rm550415@fiap.com.br>
2024-03-12 17:27:18 -0300

    Projeto Finalizado

--- src/main/java/br/com/fiap/springpgdeposito/resources/EnderecoResource.java
+++ src/main/java/br/com/fiap/springpgdeposito/resources/EnderecoResource.java
@@ -1,8 +1,7 @@
 package br.com.fiap.springpgdeposito.resources;
+import br.com.fiap.springpgdeposito.entity.Deposito;
 import br.com.fiap.springpgdeposito.entity.Endereco;
-import br.com.fiap.springpgdeposito.entity.Endereco;
-import br.com.fiap.springpgdeposito.entity.Endereco;
-import br.com.fiap.springpgdeposito.entity.Endereco;
+import br.com.fiap.springpgdeposito.repository.EnderecoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,14 +9,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Objects;
 @RestController
-@RequestMapping(value = "/edereco")
+@RequestMapping(value = "/endereco")
 public class EnderecoResource {
     @Autowired
-    private Endereco enderecoRepository;
+    private EnderecoRepository enderecoRepository;
     @GetMapping
     public List<Endereco> findAll(){return enderecoRepository.findAll();}
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Endereco> findById(@PathVariable(name = "id") int id){
+    public ResponseEntity<Endereco> findById(@PathVariable(name = "id") Long id){
         Endereco endereco = enderecoRepository.findById(id).orElse(null);
         if (Objects.isNull(endereco)){
             return ResponseEntity.notFound().build();
@@ -31,4 +30,5 @@ public class EnderecoResource {
         Endereco saved = enderecoRepository.save(endereco);
         return ResponseEntity.ok(saved);
     }
+
 }
--- src/main/java/br/com/fiap/springpgdeposito/resources/ProdutoResource.java
+++ src/main/java/br/com/fiap/springpgdeposito/resources/ProdutoResource.java
@@ -1,4 +1,41 @@
-package br.com.fhuiap.springpgdeposito.resources;
+package br.com.fiap.springpgdeposito.resources;
+import br.com.fiap.springpgdeposito.entity.Deposito;
+import br.com.fiap.springpgdeposito.entity.Endereco;
+import br.com.fiap.springpgdeposito.entity.Produto;
+import br.com.fiap.springpgdeposito.repository.EnderecoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Objects;
+@RestController
+@RequestMapping(value = "/produto")
+public class ProdutoResource {
+    @Autowired
+    private ProdutoResource produtoResource;
+    @GetMapping
+    public List<Produto> findAll(){return produtoResource.findAll();}
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Produto> findById(@PathVariable(name = "id") Long id){
+        Object o = null;
+        Produto produto = (null);
+        if (Objects.isNull(produto)){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(produto);
+    }
 
-public class  {
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Produto> persist(@RequestBody Produto produto){
+        Produto saved = produtoResource.save(produto);
+        return ResponseEntity.ok(saved);
+    }
+
+    private Produto save(Produto produto) {
+
+        return produto;
+    }
+
 }
